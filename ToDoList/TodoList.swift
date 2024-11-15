761992812f42a2069a017d4aec0c1225b665eccf
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2024-11-15 16:29:29 +0100

    Added VM

--- ToDoList/TodoList.swift
+++ ToDoList/TodoList.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class ToDoList: ObservableObject {
+    @Published var notesList = NotesList()
+    
+    var notes: [NotesList.Note] {
+        notesList.notes
+    }
+    // MARK: - Intent(s)
+
+    func selectNote(with id: UUID) {
+        notesList.selectNote(with: id)
+    }
+}
