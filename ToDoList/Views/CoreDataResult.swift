276450610b3762b5469b12ca2aa4ff19ff81dc04
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2024-11-21 02:10:45 +0300

    Added load func

---
+++ ToDoList/Views/CoreDataResult.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataResult.swift
+//  ToDoList
+//
+//  Created by Kristina Grebneva on 19.11.2024.
+//
+
+import SwiftUI
+
+struct CoreDataResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataResult()
+}
